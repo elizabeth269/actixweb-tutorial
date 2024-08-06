53e90cd29d883d702bac3cd6d148e37a6b798324
Elizbeth <elizabethadebayo269@gmail.com>
2024-08-06 16:39:15 +0100

    state

--- src/main.rs
+++ src/main.rs
@@ -26,18 +26,24 @@
 //     .await
 // }
 
-use actix_web::{web, App, HttpServer, Responder};
-
-async fn index() -> impl Responder {
-    "Helo world"
+use actix_web::{get, web, App, HttpServer};
+struct AppState {
+    app_name: String,
+}
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(web::scope("/app"))
-            .route("/index.html", web::get().to(index))
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
