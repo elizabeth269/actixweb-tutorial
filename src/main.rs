b752fa24f59ff92cdb8d6bdaea837fc343ac78d3
Elizbeth <elizabethadebayo269@gmail.com>
2024-08-08 20:40:19 +0100

    nade a few changes

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,51 @@
-// use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-// #[get("/")]
-// async fn hello() -> impl Responder {
-//     HttpResponse::Ok().body("Hello world!")
-// }
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
 
-// #[post("/echo")]
-// async fn echo(req_body: String) -> impl Responder {
-//     HttpResponse::Ok().body(req_body)
-// }
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
 
-// async fn manual_hello() -> impl Responder {
-//     HttpResponse::Ok().body("Hey there!")
-// }
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-//     HttpServer::new(|| {
-//         App::new()
-//             .service(hello)
-//             .service(echo)
-//             .route("/hey", web::get().to(manual_hello))//registering manua_hello
-//     })
-//     .bind("127.0.0.1:8080")?
-//     .run()
-//     .await
-// }
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello)) //registering manua_hello
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
 
-// use actix_web::{get, web, App, HttpServer};
-// struct AppState {
-//     app_name: String,
-// }
-// #[get("/")]
-// async fn index(data: web::Data<AppState>) -> String {
-//     let app_name = &data.app_name;
-//     format!("Hello {app_name}!")
-// }
+use actix_web::{get, web, App, HttpServer};
+struct AppState {
+    app_name: String,
+}
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}!")
+}
 
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-//     HttpServer::new(|| {
-//         App::new()
-//             .app_data(web::Data::new(AppState {
-//                 app_name: String::from("Actix Web"),
-//             }))
-//             .service(index)
-//     })
-//     .bind(("127.0.0.1", 8080))?
-//     .run()
-//     .await
-// }
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
